39926e6eb9c9e08304ee4567fa192534d71985ca
Damián Junquera <damian.junquera@equipo.es>
2017-03-12 13:20:30 +0100

    first commit

---
+++ src/main/java/com/teg/messenger/MessengerBackApplication.java
@@ -0,0 +1,12 @@
+package com.teg.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerBackApplication.class, args);
+	}
+}
