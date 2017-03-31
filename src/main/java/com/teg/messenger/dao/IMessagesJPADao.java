54c95a7607b8a0b7b61f9fd998eee6e860a92ff8
Óscar Ib <oscar.ibanez@equipo.es>
2017-03-31 20:15:15 +0200

    Implementa el Controller para guardar mensajes

--- src/main/java/com/teg/messenger/dao/IMessagesJPADao.java
+++
@@ -1,7 +0,0 @@
-package com.teg.messenger.dao;
-
-import com.teg.messenger.model.Message;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface IMessagesJPADao extends MongoRepository<Message, String> {
-}
--- src/main/java/com/teg/messenger/web/MessageController.java
+++ src/main/java/com/teg/messenger/web/MessageController.java
@@ -1,8 +1,9 @@
 package com.teg.messenger.web;
 
-import com.teg.messenger.dao.IMessagesJPADao;
 import com.teg.messenger.model.Message;
+import com.teg.messenger.repository.interfaces.MessageDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/messages")
 public class MessageController {
 
     @Autowired
-    IMessagesJPADao dao;
+    MessageDao dao;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Message> listMessages(){
+    @RequestMapping(value = "/messages", method = RequestMethod.GET)
+    public List<Message> listMessages() {
         List<Message> messages = dao.findAll();
         return messages;
     }
+
+    @RequestMapping(value = "/messages", method = RequestMethod.POST)
+    public Message addMessage(@RequestBody Message message) {
+        dao.save(message);
+        return message;
+    }
 }
+
